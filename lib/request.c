7dbaefc7edc0a983ab971248a0182278e85e9aea
Nathan LaFreniere <quitlahok@gmail.com>
2013-02-26 15:02:22 -0800

    bug fixes, support patch

--- lib/request.c
+++ lib/request.c
@@ -25,6 +25,60 @@ static int writeFn(void *buf, size_t len, size_t size, void *userdata) {
     return sLen;
 }
 
+extern struct json_object *makePatch(char *path, const char *token, char* body) {
+    CURL *curl;
+    CURLcode res;
+    struct json_object *response = NULL;
+    char* data;
+    data = (char *)calloc(sizeof(char), BUF_LEN);
+
+    int bodylen = strlen(body);
+    char* postdata;
+    postdata = (char *)calloc(sizeof(char), bodylen);
+    memcpy(postdata, body, bodylen);
+    bytesWritten = 0;
+    bytesTotal = bodylen;
+    bytesRead = 0;
+
+    struct curl_slist *headers = NULL;
+    char *tokenHeader = "Authorization: token ";
+    int headerlen = strlen(tokenHeader) + strlen(token) + 2;
+    char authHeader[headerlen];
+    snprintf(authHeader, headerlen, "%s%s", tokenHeader, token);
+    
+    int urllen = strlen(baseUrl) + strlen(path) + 2;
+    char fullUrl[urllen];
+    snprintf(fullUrl, urllen, "%s%s", baseUrl, path);
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+    if (curl) {
+        headers = curl_slist_append(headers, "Accept: application/json");
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+        headers = curl_slist_append(headers, authHeader);
+        curl_easy_setopt(curl, CURLOPT_URL, fullUrl);
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PATCH");
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postdata);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, bytesTotal);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeFn);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, data);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+        curl_global_cleanup();
+        if (res != CURLE_OK) {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+            return NULL;
+        }
+        response = json_tokener_parse(data);
+        free(data);
+        return response;
+    } else {
+        curl_global_cleanup();
+        return NULL;
+    }
+}
+
 extern struct json_object *makePostBasic(char *path, char *user, char *pass, char* body) {
     CURL *curl;
     CURLcode res;
@@ -36,6 +90,7 @@ extern struct json_object *makePostBasic(char *path, char *user, char *pass, cha
     char* postdata;
     postdata = (char *)calloc(sizeof(char), bodylen);
     memcpy(postdata, body, bodylen);
+    bytesWritten = 0;
     bytesTotal = bodylen;
     bytesRead = 0;
 
@@ -89,6 +144,7 @@ extern struct json_object *makePost(char *path, const char *token, char* body) {
     char* postdata;
     postdata = (char *)calloc(sizeof(char), bodylen);
     memcpy(postdata, body, bodylen);
+    bytesWritten = 0;
     bytesTotal = bodylen;
     bytesRead = 0;
 
